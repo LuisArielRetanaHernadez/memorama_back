139f135eccd0a09342ee406f94485b0c8bb65730
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 16:02:00 -0600

    corregiendo la exportacion de la ruta de gamer.router

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routerGame from './src/routers/game.router';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/game', routerGame)
+
+export default app;
