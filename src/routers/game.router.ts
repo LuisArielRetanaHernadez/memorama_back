daf98ebeb7fb157751591557853443ab0611a5b9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 14:32:49 -0600

    creando el archivo game.router.ts con 3 enpoint get'/', post'/' y get'/'

---
+++ src/routers/game.router.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+
+const router = express.Router();
+
+router.get('/', (req, res) => {})
+router.post('/', (req, res) => {})
+router.get('/:id', (req, res) => {})
+
+
+export default router
