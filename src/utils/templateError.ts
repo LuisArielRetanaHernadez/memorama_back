1114f1a1a39ab8a8587f10b0209665394691531e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 14:49:06 -0600

    creando la clase TemplateError para menejar los errores

---
+++ src/utils/templateError.ts
@@ -0,0 +1,12 @@
+class TemplateError extends Error {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.name = 'TemplateError';
+    this.status = status;
+    this.message = message;
+    Error.captureStackTrace(this, TemplateError);
+  }
+}
+
+export default TemplateError;
