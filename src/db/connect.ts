4be676c20380b94d99db03ef38971dca55430c66
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 21:25:25 -0600

    agregando configDotenv para hacer uso del DB_URL

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
-
+import { configDotenv } from 'dotenv'
+configDotenv()
 export const connectDB = async (): Promise<any> => {
   try {
-    const conn = await mongoose.connect('.....')
+    const conn = await mongoose.connect(process.env.BD_URL as string)
     return conn
   } catch (error) {
     console.log(error)
