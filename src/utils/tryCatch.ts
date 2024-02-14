b6450f1b3de60712d8f0c32dedbaa0d0010820ee
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 23:35:27 -0600

    agregando tipado a la funcion tryCatch

--- src/utils/tryCatch.ts
+++ src/utils/tryCatch.ts
@@ -1,5 +1,13 @@
-const tryCatch = (fn: Function) => (req: any, res: any, next: any) => {
-  return Promise.resolve(fn(req, res, next)).catch(next);
+import { Request, Response, NextFunction } from 'express'
+
+const tryCatch = async <T>(
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<T>
+): Promise<any> => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await fn(req, res, next)
+  } catch (error) {
+    next(error)
+  }
 }
 
-export default tryCatch;
+export default tryCatch
