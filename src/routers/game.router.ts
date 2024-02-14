ce88f604ac590410d4994e49bd7cc21c97ed1fd5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-14 14:57:35 -0600

    agregadno el controlador createGame en el enpoint post '/'

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -1,11 +1,12 @@
-import * as express from 'express';
+import * as express from 'express'
 
+// controller game
+import { createGame } from '../controllers/game.controller'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res) => {})
-router.post('/', (req, res) => {})
-router.get('/:id', (req, res) => {})
-
+// router.get('/', (req, res) => {})
+router.post('/', createGame)
+// router.get('/:id', (req, res) => {})
 
 export default router
