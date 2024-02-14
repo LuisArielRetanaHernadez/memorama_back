0f0e8605b22c2a09ba5d7bf89209207ece33b76e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 20:05:50 -0600

    creando el archivo game.controller.ts con el controlador createGame

---
+++ src/controllers/game.controller.ts
@@ -0,0 +1,6 @@
+import tryCatch from '../utils/tryCatch'
+import TemplateError from '../utils/templateError'
+
+export const createGame = tryCatch(async (_req: any, _res: any, _next: any) => {
+
+})
