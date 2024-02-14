f0b2b731cbbf7cd53af1cfdd77db50a3890f6aac
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-14 15:03:46 -0600

    agregando guin abajo en el parametro next del controlador createGame

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -6,7 +6,7 @@ import { Request, Response, NextFunction } from 'express'
 import { addGame } from '../service/gameService'
 import { ResponseJson, StatusResponse, sendReponseJson } from '../types/game.types'
 
-export const createGame = tryCatch(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const createGame = tryCatch(async (req: Request, res: Response, _next: NextFunction): Promise<any> => {
   const {
     title,
     category,
