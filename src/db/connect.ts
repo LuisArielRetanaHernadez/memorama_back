9a96d2a9522be5d395940a97945faaa1fd8b4579
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-14 15:39:26 -0600

    agregando configuracion en el configDotenv para acceder a las variables del archivo .development.env

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+
+// dotenv
 import { configDotenv } from 'dotenv'
-configDotenv()
+configDotenv({ path: './.development.env' })
+
 export const connectDB = async (): Promise<any> => {
   try {
-    const conn = await mongoose.connect(process.env.BD_URL as string)
+    const conn = await mongoose.connect(process.env.DB_URL as string)
     return conn
   } catch (error) {
     console.log(error)
