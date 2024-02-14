b625c69019f79f2a856dbe2f98b26e3e56fa728c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-13 20:34:16 -0600

    creando la interfaz Game con los type PrivateGame, OnlineGame, Category y LavelGame

---
+++ src/types/game.types.ts
@@ -0,0 +1,14 @@
+export type PrivateGame = 'private' | 'public'
+export type OnlineGame = 'online' | 'offline'
+export type CategoryGame = 'numbers' | 'animals' | 'science'
+export type LavelGame = 'easy' | 'medium' | 'hard'
+
+export interface Game {
+  title: string
+  playerLimit: string
+  category: CategoryGame
+  isOnline: OnlineGame
+  isPrivate: PrivateGame
+  players: string[]
+  lavel: LavelGame
+}
