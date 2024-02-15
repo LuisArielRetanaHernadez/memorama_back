074022b1794b87a3a69920a57202ad8a820054ba
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-15 14:20:32 -0600

    creando el type Category

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,7 @@ import { Response } from 'express'
 
 export type LavelGame = 'easy' | 'medium' | 'hard'
 export type StatusSource = 'live' | 'update' | 'deleted'
+export type CategoryGame = 'numbers' | 'animals' | 'programmation' | 'countries'
 
 export type StatusResponse = 200 | 201 | 202 | 203 | 204 | 400 | 404
 export type StatusResponseMessage = 'OK' | 'Created' | 'Accepted' | 'No Content' | 'Bad Request' | 'Not Found'
