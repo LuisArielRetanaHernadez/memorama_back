ce7e4bdf9de76dc0ad796f2009547f682ac3151f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-15 12:29:32 -0600

    quitando los punto y goma

--- src/utils/templateError.ts
+++ src/utils/templateError.ts
@@ -1,12 +1,12 @@
 class TemplateError extends Error {
-  status: number;
-  constructor(message: string, status: number) {
-    super(message);
-    this.name = 'TemplateError';
-    this.status = status;
-    this.message = message;
-    Error.captureStackTrace(this, TemplateError);
+  status: number
+  constructor (message: string, status: number) {
+    super(message)
+    this.name = 'TemplateError'
+    this.status = status
+    this.message = message
+    Error.captureStackTrace(this, TemplateError)
   }
 }
 
-export default TemplateError;
+export default TemplateError
