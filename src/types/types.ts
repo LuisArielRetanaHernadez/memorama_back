f1ffad6090e347c8f79e412cb6b1c4da0cf8e4d4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-15 13:19:59 -0600

    agregando los types StatusReponse y StatusResponseMessage, ResponseJson y la funcion sendResponseJson

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,18 @@
+import { Response } from 'express'
+
 export type LavelGame = 'easy' | 'medium' | 'hard'
 export type StatusSource = 'live' | 'update' | 'deleted'
+
+export type StatusResponse = 200 | 201 | 202 | 203 | 204 | 400 | 404
+export type StatusResponseMessage = 'OK' | 'Created' | 'Accepted' | 'No Content' | 'Bad Request' | 'Not Found'
+
+export interface ResponseJson {
+  status: StatusResponseMessage
+  data: Object
+}
+
+export const sendReponseJson = (
+  res: Response,
+  content: ResponseJson, status: StatusResponse): any => {
+  return res.status(status).json(content)
+}
