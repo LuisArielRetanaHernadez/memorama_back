d79d3e8389664ae89ad228ffa527750e8ecc14f8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-15 14:21:25 -0600

    creando la propiedad category tipandolo con el type Category

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -1,8 +1,9 @@
-import { StatusSource, LavelGame } from './types'
+import { StatusSource, LavelGame, CategoryGame } from './types'
 
 export interface Card {
   content: String
   pair: Number
   lavel: LavelGame
+  category: CategoryGame
   status: StatusSource
 }
