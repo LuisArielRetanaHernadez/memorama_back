b29a4081dc4a0f0dd99ebf47b0cb2b46cf9a86a5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-16 14:54:21 -0600

    creando el router '?search' con el controlador searchGame

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 
 // controller game
-import { createGame, getGameById } from '../controllers/game.controller'
+import { createGame, getGameById, searchGame } from '../controllers/game.controller'
 
 const router = express.Router()
 
 router.post('/', createGame)
 router.get('/:id', getGameById)
+// router para buscar games por la variable search en la consulta de la url
+router.get('?search', searchGame)
 
 export default router
