83b132748da53a996b633340a9f99d905eef8969
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-16 14:38:31 -0600

    creando el router con la ruta '/:id' con el controlador getGameById

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -1,12 +1,11 @@
 import * as express from 'express'
 
 // controller game
-import { createGame } from '../controllers/game.controller'
+import { createGame, getGameById } from '../controllers/game.controller'
 
 const router = express.Router()
 
-// router.get('/', (req, res) => {})
 router.post('/', createGame)
-// router.get('/:id', (req, res) => {})
+router.get('/:id', getGameById)
 
 export default router
