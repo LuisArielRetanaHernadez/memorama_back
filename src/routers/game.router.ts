416f486482c976f369dc26fc92ee20742ef65ded
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-16 14:55:31 -0600

    quitando el comentario del router '?search'

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -7,7 +7,6 @@ const router = express.Router()
 
 router.post('/', createGame)
 router.get('/:id', getGameById)
-// router para buscar games por la variable search en la consulta de la url
 router.get('?search', searchGame)
 
 export default router
