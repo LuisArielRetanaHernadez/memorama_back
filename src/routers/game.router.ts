54b39d42a905f7b8e9e808ef43108fc977b27c57
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-16 16:03:06 -0600

    creadno el router get '/?limit=&page=' con el controlador getGame

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -1,11 +1,12 @@
 import * as express from 'express'
 
 // controller game
-import { createGame, getGameById, searchGame } from '../controllers/game.controller'
+import { createGame, getGame, getGameById, searchGame } from '../controllers/game.controller'
 
 const router = express.Router()
 
 router.post('/', createGame)
+router.get('/?limit=&page=', getGame)
 router.get('/:id', getGameById)
 router.get('/game?search', searchGame)
 
