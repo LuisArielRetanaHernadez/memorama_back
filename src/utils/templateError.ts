87613d1f9bd988593055822c11e5cc3a332ce308
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-17 14:28:56 -0600

    tipando la propiedad status de la clase TemplateError

--- src/utils/templateError.ts
+++ src/utils/templateError.ts
@@ -1,11 +1,13 @@
+import { StatusResponse } from '../types/types'
+
 class TemplateError extends Error {
-  status: number
-  constructor (message: string, status: number) {
+  status: StatusResponse
+  constructor (message: string, status: StatusResponse) {
     super(message)
     this.name = 'TemplateError'
     this.status = status
     this.message = message
-    Error.captureStackTrace(this, TemplateError)
+    Error.captureStackTrace(this, this.constructor)
   }
 }
 
