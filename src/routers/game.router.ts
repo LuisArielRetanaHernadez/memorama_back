362f82513c1e3c38c5f097d763412a8c2d5f76e6
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-17 23:22:04 -0600

    agregando el middleware createGameSchema al router post '/'

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -2,10 +2,11 @@ import * as express from 'express'
 
 // controller game
 import { createGame, getGame, getGameById, searchGame } from '../controllers/game.controller'
+import { createGameSchema } from '../middlewares/gameBody.middleware'
 
 const router = express.Router()
 
-router.post('/', createGame)
+router.post('/', createGameSchema, createGame)
 router.get('/', getGame)
 router.get('/id/:id', getGameById)
 router.get('/search', searchGame)
