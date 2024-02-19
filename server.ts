85190be9014bfe225f241f773ce5e413eb00818d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-19 15:25:24 -0600

    llamando la funcion gameSpace en el server con el io

--- server.ts
+++ server.ts
@@ -4,14 +4,15 @@ import { connectDB } from './src/db/connect'
 // socket io
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import { gameSpace } from './src/namesSpace/game.space'
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, {})
 
-io.on('connection', _socket => {
-  console.log('a user connected')
-})
-
+// io.on('connection', _socket => {
+//   console.log('a user connected')
+// })
+gameSpace(io)
 const PORT = 3333
 
 httpServer.listen(PORT, () => {
