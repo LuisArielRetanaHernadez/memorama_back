e9c82f243bc93035745861758b6e9012319b56bd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-19 12:48:01 -0600

    configurando el servidor con el socket.io

--- server.ts
+++ server.ts
@@ -1,8 +1,16 @@
 import app from './app'
 import { connectDB } from './src/db/connect'
 
+// socket io
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+
 const PORT = 3333
-app.listen(PORT, () => {
+
+httpServer.listen(PORT, () => {
   connectDB().then(() => console.log('Database connected')).catch(console.error)
   console.log('Server is running on port 3333')
 })
