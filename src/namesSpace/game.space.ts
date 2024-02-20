87e055bb20d43e22c4e492262e59236748216107
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-20 14:53:09 -0600

    agregando el middleware apply al espacio de nombre game

--- src/namesSpace/game.space.ts
+++ src/namesSpace/game.space.ts
@@ -2,11 +2,16 @@ import { Socket } from 'socket.io'
 import gameSchema from '../schemas/game.schema'
 import { NewGame } from '../types/game.types'
 
+// middleware
+import { apllyMiddleware } from '../middlewares/socket/apply.middleware'
+
 export const gameSpace = (io: any): any => {
   const game = io.of('/game')
 
   game.on('connection', (socket: Socket) => {
     console.log('a user connected to game space ', socket.id)
+    socket.use(apllyMiddleware)
+
     socket.on('disconnect', () => {
       console.log('user disconnected from game space')
     })
@@ -36,6 +41,9 @@ export const gameSpace = (io: any): any => {
       await gameFind?.save()
       await socket.join(id)
       socket.emit('game join', gameFind)
+    })
+    socket.on('message', async () => {
+      game.emit('message', 'hola')
     })
   })
 }
