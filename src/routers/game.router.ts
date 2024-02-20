0fec4c122e6451614f4d2df7a50060a568aa951a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-19 20:46:29 -0600

    agregando el middleware validate para envolver el middleware createGameSchema

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -3,10 +3,11 @@ import * as express from 'express'
 // controller game
 import { createGame, getGame, getGameById, searchGame } from '../controllers/game.controller'
 import { createGameSchema } from '../middlewares/gameBody.middleware'
+import { validate } from '../middlewares/validate.middleware'
 
 const router = express.Router()
 
-router.post('/', createGameSchema, createGame)
+router.post('/', validate(createGameSchema), createGame)
 router.get('/', getGame)
 router.get('/id/:id', getGameById)
 router.get('/search', searchGame)
