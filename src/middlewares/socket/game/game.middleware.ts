ab5ea2a62407e3a50fcdc61d31d12d2e615262a0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-22 14:12:23 -0600

    eliminando el middleware para evento 'create game'

--- src/middlewares/socket/game/game.middleware.ts
+++ src/middlewares/socket/game/game.middleware.ts
@@ -1,72 +1,8 @@
 import { Socket } from 'socket.io'
-import { NewGame } from '../../../types/game.types'
 import gameSchema from '../../../schemas/game.schema'
 // import { Player } from '../../../types/types'
 
 export const gameMiddleware = {
-  'game create': (_socket: Socket, data: NewGame, next: any) => {
-    const titleValid = data.title
-    if (typeof titleValid !== 'string') {
-      return next(new Error('Title is not a string'))
-    }
-    if (titleValid.length < 0) {
-      return next(new Error('Title is too short'))
-    }
-    const categoryValid = data.category
-    if (typeof categoryValid !== 'string') {
-      return next(new Error('Category is not a string'))
-    }
-    if (categoryValid.length < 0) {
-      return next(new Error('Category is too short'))
-    }
-    const playerLimitValid = data.playerLimit
-    if (typeof playerLimitValid !== 'number') {
-      return next(new Error('Player limit is not a number'))
-    }
-    if (playerLimitValid < 2) {
-      return next(new Error('Player limit is too low'))
-    }
-    if (playerLimitValid > 4) {
-      return next(new Error('Player limit is too high'))
-    }
-    const isOnlineValid = data.isOnline
-    if (typeof isOnlineValid !== 'boolean') {
-      return next(new Error('Is online is not a boolean'))
-    }
-    const isPrivateValid = data.isPrivate
-    if (typeof isPrivateValid !== 'boolean') {
-      return next(new Error('Is private is not a boolean'))
-    }
-    if (isPrivateValid && isOnlineValid) {
-      return next(new Error('Private game can only be online'))
-    }
-    const passwordValid = data.password
-    if (isPrivateValid) {
-      if (typeof passwordValid !== 'string') {
-        return next(new Error('Password is not a string'))
-      }
-      if (isPrivateValid && passwordValid.length < 6) {
-        return next(new Error('Password is too short'))
-      }
-    }
-
-    const playersValid = data.players
-    const playersNameValid = playersValid.some(player => typeof player.name === 'string' && player.name.length < 1)
-    if (playersNameValid) {
-      return next(new Error('Player name is too short and must be string'))
-    }
-    if (playersValid.length < 1) {
-      return next(new Error('Player limit is too low'))
-    }
-    if (playersValid.length !== 1 && isOnlineValid) {
-      return next(new Error('Private game must have at least 2 or more players'))
-    }
-    if (playersValid.length > playerLimitValid) {
-      return next(new Error('Player limit is too high'))
-    }
-
-    return next()
-  },
   'game join': async (_socket: Socket, data: any, next: any) => {
     const id = data.id
     console.log('join game ', data)
